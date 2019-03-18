99fc648e648c193499fee92be004db22085e3178
Saahil Ali <programmer290399@gmail.com>
2019-03-18 20:05:20 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+
+# posts =  [
+#     {
+#         'author': "Saahil",
+#         'title' : 'Blog Post 1',
+#         'content':'First post content',
+#         'date_posted':'march 27, 2019'  
+#     },
+#     {
+#         'author': "Motes",
+#         'title' : 'Blog Post 2',
+#         'content':'Second post content',
+#         'date_posted':'march 28, 2019'  
+#     }
+# ]   
+def home(request):
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
